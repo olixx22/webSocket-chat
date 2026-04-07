a7994e3f5a719da02f6016ebf3a3ee0f5df21e13
olixx22 <sashik.bykov@icloud.com>
2026-04-07 22:20:43 +0500

    attempt to make the main.py asynchronous

--- auth-service/main.py
+++ auth-service/main.py
@@ -1,14 +1,15 @@
 import grpc
-from concurrent import futures
 from dotenv import load_dotenv
 import os
 import shared.gen.py.auth_pb2 as auth_pb2
 import shared.gen.py.auth_pb2_grpc as auth_pb2_grpc
 from shared.jwt import create_access_token, decode_token
 load_dotenv()
+import asyncio
+from grpc.aio import server
 
 class AuthService(auth_pb2_grpc.AuthServiceServicer):
-    def Register(self, request, context):
+    async def Register(self, request, context):
         user_id = "test-uuid-12345"
         email = request.email
         password = request.password
@@ -24,7 +25,7 @@ class AuthService(auth_pb2_grpc.AuthServiceServicer):
 
 
 
-    def Login(self, request, context):
+    async def Login(self, request, context):
         user_id = "test-uuid-12345"
         email = request.email
         password = request.password
@@ -37,10 +38,10 @@ class AuthService(auth_pb2_grpc.AuthServiceServicer):
             refresh_token=refresh
         )
 
-    def RefreshToken(self, request, context):
+    async def RefreshToken(self, request, context):
         user_id = decode_token(request.refresh_token, expected_type='refresh')
 
-        if user_id == 'неа':
+        if user_id == None:
             context.abort(grpc.StatusCode.UNAUTHENTICATED, "Token invalid or expired")
 
         new_access = create_access_token(user_id, token_type='access', expires_in=1)
@@ -51,15 +52,13 @@ class AuthService(auth_pb2_grpc.AuthServiceServicer):
             user_id=user_id,
             refresh_token=new_refresh
         )
-def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    auth_pb2_grpc.add_AuthServiceServicer_to_server(AuthService(), server)
+async def serve():
 
+    auth_pb2_grpc.add_AuthServiceServicer_to_server(AuthService(), server())
     server.add_insecure_port('[::]:50051')
-    print("Сервер запущен на порту 50051...")
-    server.start()
-    server.wait_for_termination()
+    await server().start()
+    await server().wait_for_termination()
 
 
 if __name__ == '__main__':
-    serve()
+    asyncio.run(serve())
